e355ff97c4ce10ee5710f20fe248494728467590
nkhakwan <post_khan@yahoo.com>
2020-06-01 10:54:36 -0700

    few migrations files that we created and a minor change to Category.cs file

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -14,3 +14,22 @@ namespace ToDoList.Models
         public virtual ICollection<CategoryItem> Items { get; set; }
     }
 }
+
+
+
+
+public ActionResult Index()
+{
+    return View(_db.Items.ToList());
+}
+
+
+
+/*public ActionResult Details(int id)
+{
+    var thisItem = _db.Items go into the items table
+        .Include(item => item.Categories) find all CategoryItem objects associated with the item
+        .ThenInclude(join => join.Category) find the Category object that belongs to each CategoryItem
+        .FirstOrDefault(item => item.ItemId == id); for the item that matches the id passed into this route
+    return View(thisItem);
+}*/
