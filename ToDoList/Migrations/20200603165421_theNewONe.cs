9febeb540b942e5e3bcdfe8326b342c19be41b1b
nkhakwan <post_khan@yahoo.com>
2020-06-03 10:06:00 -0700

    migrations once more and build DB afresh

---
+++ ToDoList/Migrations/20200603165421_theNewONe.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class theNewONe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
